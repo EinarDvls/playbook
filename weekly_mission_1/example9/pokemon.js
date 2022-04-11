af39569bcac8b4d7c4b1ad599f09bfbed3d3d539
Einar Davalos <99060661+EinarDvls@users.noreply.github.com>
2022-04-10 19:36:37 -0500

    Update pokemon.js

--- weekly_mission_1/example9/pokemon.js
+++ weekly_mission_1/example9/pokemon.js
@@ -1,1 +1,13 @@
-class Pokemon
+export default class Pokemon{
+  constructor(name){
+    this.name = name
+  }
+
+  sayHello(){
+    console.log(`Mi pokemon ${this.name} te saluda!!!`)
+  }
+
+  sayMessage(msg){
+    console.log(`Mi pokemon ${this.name} dice: ${msg}`)
+  }
+}
