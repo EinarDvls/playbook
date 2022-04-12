e5dc8fc605f5aea85458ef4a0770685f4ef61233
EinarDvls <launchx06534@innovaccion.mx>
2022-04-12 16:07:30 -0500

    1. Objetos

---
+++ weekly_mission_2/0_objetos.js/Ejemplo 2.js
@@ -0,0 +1,7 @@
+// Ejemplo 2: Crear un objeto con propiedades
+const myObject2 = {
+    name: "Einar",
+    age: 29
+}
+console.log("Ejemplo 2: Crear un objeto con propiedades");
+console.log(myObject2);
---
+++ weekly_mission_2/0_objetos.js/Ejemplo 3.js
@@ -0,0 +1,19 @@
+// Ejemplo 3: Objeto con diferentes propiedades
+const myObject3 = {
+    name: "Einar",
+    age: 29,
+    nicknames: [
+     "Mamon",
+      "Gordo",
+      "Prieto"
+    ],
+    address: {
+      zip_code: "45403",
+      street: "Av. Rio de la plata",
+      city: "Guadalajra"
+    }
+  }
+  console.log("Ejemplo 3: Objeto con diferentes propiedades")
+  console.log(myObject3)
+  console.log(myObject3.name)
+  console.log(myObject3["address"])
